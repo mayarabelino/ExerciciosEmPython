ac03b992cfd39a5cab2e99c5a838d1f4d55bdd3a
mayarabelino <mayarabr99@gmail.com>
2024-10-20 20:12:53 -0300

    exercicios em Python

---
+++ projetos/ex074.py
@@ -0,0 +1,12 @@
+# Crie um programa que vai gerar 5 números aleatórios e colocar em uma tupla.
+# Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla
+
+from random import randint
+a1 = randint(0,10)
+a2 = randint(0,10)
+a3 = randint(0,10)
+a4 = randint(0,10)
+a5 = randint(0,10)
+aleatorio = (a1, a2, a3, a4, a5)
+
+print(aleatorio)
